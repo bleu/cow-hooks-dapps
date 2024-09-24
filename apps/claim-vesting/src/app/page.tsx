b00de040326cc01321ee00a85dea632aef965a87
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-09-24 15:48:59 -0300

    revert claim vesting hook changes

--- apps/claim-vesting/src/app/page.tsx
+++ apps/claim-vesting/src/app/page.tsx
@@ -1,22 +1,5 @@
-"use client";
-
-import { TokenLogo } from "@bleu/cow-hooks-ui";
-import {
-  type HookDappContext,
-  initCoWHookDapp,
-} from "@cowprotocol/hook-dapp-lib";
-import { StrictMode, useEffect, useState } from "react";
+import { Joana } from "@bleu/cow-hooks-ui";
 
 export default function Page() {
-  const [context, setContext] = useState<HookDappContext | null>(null);
-
-  useEffect(() => {
-    const { actions, provider } = initCoWHookDapp({ onContext: setContext });
-  }, []);
-
-  return (
-    <StrictMode>
-      <p>This is a test</p>
-    </StrictMode>
-  );
+  return <Joana myProp="123" />;
 }
