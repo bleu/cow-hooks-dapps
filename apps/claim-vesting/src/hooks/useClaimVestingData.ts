f9f09502eeca2532f5c54b40363787aa797e8221
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-25 17:45:30 -0300

    refactor: add hooks for reading vesting and token contracts data

--- apps/claim-vesting/src/hooks/useClaimVestingData.ts
+++ apps/claim-vesting/src/hooks/useClaimVestingData.ts
@@ -1,12 +1,11 @@
-import useSWR from "swr";
-import { formatUnits, isAddress, type Address } from "viem";
-import { VestingEscrowAbi } from "../abis/VestingEscrowAbi";
-import { erc20Abi } from "../abis/erc20Abi";
+import { formatUnits, isAddress } from "viem";
 
-import { mainnet, gnosis, sepolia, arbitrum } from "viem/chains";
-import { createPublicClient, http } from "viem";
-import { SupportedChainId } from "@cowprotocol/cow-sdk";
-import { useCallback, useEffect, useMemo } from "react";
+import type { SupportedChainId } from "@cowprotocol/cow-sdk";
+import { useMemo } from "react";
+import { http, createPublicClient } from "viem";
+import { arbitrum, gnosis, mainnet, sepolia } from "viem/chains";
+import { useReadToken } from "./useReadToken";
+import { useReadVesting } from "./useReadVesting";
 
 interface useClaimVestingDataParams {
   chainId: SupportedChainId | undefined;
@@ -38,134 +37,21 @@ export const useClaimVestingData = ({
         chain: chainMapping[chainId],
         transport: http(),
       }),
-    [chainId]
-  );
-
-  /// ---------------- VESTING DATA ---------------- ///
-
-  const readVesting = useCallback(
-    async (address: Address) => {
-      const vestingContract = {
-        address: address,
-        abi: VestingEscrowAbi,
-      } as const;
-
-      const vestingResults =
-        publicClient &&
-        (await publicClient.multicall({
-          contracts: [
-            {
-              ...vestingContract,
-              functionName: "unclaimed",
-            },
-            {
-              ...vestingContract,
-              functionName: "recipient",
-            },
-            {
-              ...vestingContract,
-              functionName: "token",
-            },
-          ],
-        }));
-
-      vestingResults?.forEach((result) => {
-        const status = result?.status;
-        const error = result?.error;
-
-        // When the address is not a vesting contract
-        if (
-          status === "failure" &&
-          error?.name === "ContractFunctionExecutionError"
-        )
-          throw new Error("Address is not a valid vesting contract");
-
-        // Other unexpected errors
-        if (status === "failure") throw new Error("Unexpected error");
-      });
-
-      return {
-        claimableAmountWei: vestingResults && vestingResults[0],
-        recipient: vestingResults && vestingResults[1],
-        tokenAddress: vestingResults && vestingResults[2],
-      };
-    },
-    [publicClient]
-  );
-
-  const shouldFetchVesting = isAddress(debouncedAddress);
-
-  const {
-    data: vestingData,
-    isLoading: isLoadingVesting,
-    error: errorVesting,
-  } = useSWR(shouldFetchVesting ? debouncedAddress : null, readVesting, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    refreshWhenOffline: false,
-    refreshWhenHidden: false,
-    refreshInterval: 0,
-  });
-
-  const claimableAmountWei = vestingData?.claimableAmountWei?.result;
-  const recipient = vestingData?.recipient?.result;
-  const tokenAddress = vestingData?.tokenAddress?.result;
-
-  /// ---------------- TOKEN DATA ---------------- ///
-
-  const readToken = useCallback(
-    async (address: Address) => {
-      const tokenContract = {
-        address: address,
-        abi: erc20Abi,
-      } as const;
-
-      const tokenResults =
-        publicClient &&
-        (await publicClient.multicall({
-          contracts: [
-            {
-              ...tokenContract,
-              functionName: "symbol",
-            },
-            {
-              ...tokenContract,
-              functionName: "decimals",
-            },
-          ],
-        }));
-
-      tokenResults?.forEach((result) => {
-        // Unexpected errors with token
-        if (result.status === "failure") throw new Error("Unexpected error");
-      });
-
-      return {
-        symbol: tokenResults && tokenResults[0],
-        decimals: tokenResults && tokenResults[1],
-      };
-    },
-    [publicClient]
+    [chainId],
   );
 
   const {
-    data: tokenData,
-    isLoading: isLoadingToken,
-    error: errorToken,
-  } = useSWR(tokenAddress ? tokenAddress : null, readToken, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    refreshWhenOffline: false,
-    refreshWhenHidden: false,
-    refreshInterval: 0,
+    claimableAmountWei,
+    recipient,
+    tokenAddress,
+    isLoadingVesting,
+    errorVesting,
+  } = useReadVesting({ publicClient, debouncedAddress });
+
+  const { tokenSymbol, decimals, isLoadingToken, errorToken } = useReadToken({
+    publicClient,
+    tokenAddress,
   });
-
-  const tokenSymbol = tokenData?.symbol?.result
-    ? String(tokenData.symbol.result)
-    : "";
-  const decimals = tokenData?.decimals?.result;
-
-  /// -------------------------------------------- ///
 
   const errorMessage = getErrorMessage({
     debouncedAddress,
