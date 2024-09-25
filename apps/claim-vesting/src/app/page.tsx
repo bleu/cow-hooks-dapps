49dd0f4d00600012e38139a795e45447f26ba787
Jean Neiverth <jeanneiverth@gmail.com>
2024-09-25 12:00:35 -0300

    send ClaimVesting app to page

--- apps/claim-vesting/src/app/page.tsx
+++ apps/claim-vesting/src/app/page.tsx
@@ -2,6 +2,16 @@
 
 import { useTheme } from "./ThemeContext";
 
+import { useDebounceValue } from "../hooks/useDebounceValue";
+
+import {
+  CoWHookDappActions,
+  HookDappContext,
+  initCoWHookDapp,
+} from "@cowprotocol/hook-dapp-lib";
+import { Web3Provider } from "@ethersproject/providers";
+import type { Signer } from "@ethersproject/abstract-signer";
+
 import {
   ClaimableAmountContainer,
   Wrapper,
@@ -9,9 +19,35 @@ import {
   AddressInput,
   ButtonPrimary,
 } from "@bleu/cow-hooks-ui";
+import { useEffect, useState } from "react";
+
+import { useClaimVestingData } from "../hooks/useClaimVestingData";
 
 export default function Page() {
   const { theme, toggleTheme } = useTheme();
+  const [actions, setActions] = useState<CoWHookDappActions | null>(null);
+  const [context, setContext] = useState<HookDappContext | null>(null);
+  const [signer, setSigner] = useState<Signer | null>(null);
+
+  const { account, orderParams, chainId } = context || {};
+
+  const [typedAddress, setTypedAddress] = useState("");
+
+  const [debouncedAddress] = useDebounceValue(typedAddress, 300, {
+    leading: true,
+  });
+
+  useEffect(() => {
+    const { actions, provider } = initCoWHookDapp({ onContext: setContext });
+    const web3Provider = new Web3Provider(provider);
+    const signer = web3Provider.getSigner();
+
+    setActions(actions);
+    setSigner(signer);
+  }, []);
+
+  const { errorMessage, formattedClaimableAmount, tokenSymbol, loading } =
+    useClaimVestingData({ chainId, account, debouncedAddress });
 
   return (
     <Wrapper>
@@ -19,17 +55,29 @@ export default function Page() {
         Switch to {theme === "light" ? "Dark" : "Light"} Theme
       </button>
       <ContentWrapper>
-        <AddressInput theme={theme} label="Place vesting contract address" />
+        <AddressInput
+          onChange={(e) => setTypedAddress(e.target.value)}
+          theme={theme}
+          label="Place vesting contract address"
+        />
         <div className="flex flex-col w-full">
           <ClaimableAmountContainer>
             <span>Total Available to claim</span>
-            <span>0,0</span>
+            <span>
+              {formattedClaimableAmount} {tokenSymbol && tokenSymbol}
+            </span>
           </ClaimableAmountContainer>
         </div>
       </ContentWrapper>
-      <ButtonPrimary>
-        <span>Add hook</span>
-      </ButtonPrimary>
+      {errorMessage ? (
+        <span className="text-center my-[25px]">{errorMessage}</span>
+      ) : loading ? (
+        <span className="text-center my-[25px]">Loading...</span>
+      ) : (
+        <ButtonPrimary disabled={debouncedAddress === ""}>
+          <span>Add hook</span>
+        </ButtonPrimary>
+      )}
     </Wrapper>
   );
 }
