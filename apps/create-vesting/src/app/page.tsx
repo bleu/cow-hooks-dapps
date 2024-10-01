9fee8eed467b70bc73536b77d4274a1fbc111796
Jean Neiverth <jeanneiverth@gmail.com>
2024-10-01 10:17:42 -0300

    feat: add period button to create-vesting

--- apps/create-vesting/src/app/page.tsx
+++ apps/create-vesting/src/app/page.tsx
@@ -6,10 +6,8 @@ import {
   initCoWHookDapp,
 } from "@cowprotocol/hook-dapp-lib";
 
-import { ChevronDownIcon } from "@radix-ui/react-icons";
-
 import { ButtonPrimary, ContentWrapper, Wrapper } from "@bleu/cow-hooks-ui";
-import { AddressInput } from "form-ui/AddressInput";
+import { PeriodWithScaleInput } from "form-ui/PeriodWithScaleInput";
 import { useEffect, useState } from "react";
 
 import { Input } from "@bleu/cow-hooks-ui";
@@ -25,28 +23,41 @@ import { useForm, useWatch } from "react-hook-form";
 import { isAddress } from "viem";
 import { z } from "zod";
 
+const periodScaleOptions = ["Day", "Week", "Month"];
+
+const scaleToSecondsMapping = {
+  Day: 24 * 60 * 60,
+  Week: 7 * 24 * 60 * 60,
+  Month: 30 * 24 * 60 * 60,
+};
+
+const refinePeriodScale = (value: string) => {
+  return periodScaleOptions.includes(value);
+};
+
 export const createVestingSchema = z.object({
   recipient: z
     .string()
     .min(1, "Address is required")
-    .refine((value: string) => {
-      console.log("refining", value);
-      return value === "" || isAddress(value);
-    }, "Insert a valid Ethereum address"),
+    .refine(isAddress, "Insert a valid Ethereum address"),
+  period: z.number().gt(0),
+  periodScale: z
+    .string()
+    .refine(refinePeriodScale, "Scale must be one of the options"),
 });
 
 export default function Page() {
-  // const [actions, setActions] = useState<CoWHookDappActions | null>(null);
-  // const [context, setContext] = useState<HookDappContext | null>(null);
+  const [actions, setActions] = useState<CoWHookDappActions | null>(null);
+  const [context, setContext] = useState<HookDappContext | null>(null);
 
-  // @ts-ignore
-  // const isDarkMode = context?.isDarkMode;
-  // const { account, chainId } = context || {};
+  const isDarkMode = context?.isDarkMode;
+  const { account, chainId } = context || {};
 
   const form = useForm<typeof createVestingSchema._type>({
     resolver: zodResolver(createVestingSchema),
     defaultValues: {
-      recipient: "",
+      period: 1,
+      periodScale: "Day",
     },
   });
 
@@ -54,18 +65,15 @@ export default function Page() {
   const { recipient } = useWatch({ control });
   const { errors, isValid } = formState;
 
-  const [open, setOpen] = useState(false);
-  const [periodScale, setPeriodScale] = useState("Days");
+  useEffect(() => {
+    const { actions } = initCoWHookDapp({ onContext: setContext });
+    setActions(actions);
+  }, []);
 
-  // useEffect(() => {
-  //   const { actions } = initCoWHookDapp({ onContext: setContext });
-  //   setActions(actions);
-  // }, []);
-
-  // useEffect(() => {
-  //   const newTheme = isDarkMode ? "dark" : "light";
-  //   document.documentElement.setAttribute("data-theme", newTheme);
-  // }, [isDarkMode]);
+  useEffect(() => {
+    const newTheme = isDarkMode ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", newTheme);
+  }, [isDarkMode]);
 
   // useEffect(() => {
   //   console.log("errors", errors);
@@ -88,72 +96,39 @@ export default function Page() {
   };
 
   function onSubmit(data: typeof createVestingSchema._type) {
-    // if (!context) return;
+    if (!context) return;
     console.log("data", data);
     addHook();
   }
 
-  const periodScaleOptions = ["Days", "Weeks", "Months"];
-
   return (
     <>
-      {/* {context && ( */}
-      <Form {...form} className="contents">
-        <Wrapper>
-          <ContentWrapper>
-            <Input
-              name="recipient"
-              label="Recipient"
-              placeholder="0xabc..."
-              autoComplete="off"
-              className="w-full mt-0 p-2.5 rounded-xl outline-none text-color-text-paper border-2 border-color-border bg-color-paper-darker"
-            />
-            <br />
-            <div className="flex w-48">
+      {context && (
+        <Form {...form} className="contents">
+          <Wrapper>
+            <ContentWrapper>
               <Input
-                name="period"
-                type="number"
+                name="recipient"
+                label="Recipient"
+                placeholder="0xabc..."
                 autoComplete="off"
-                className="outline-none text-right w-24 h-10 p-2.5 rounded-l-xl rounded-r-none text-base text-color-text-paper border-2 border-color-border bg-color-paper [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                className="w-full mt-0 p-2.5 rounded-xl outline-none text-color-text-paper border-2 border-color-border bg-color-paper-darker"
               />
-              <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger className="w-full">
-                  <div className="flex flex-col">
-                    <div
-                      className="w-24 h-10 p-2.5 flex items-center justify-between border-2 border-l-0 rounded-r-xl text-color-text-paper border-color-border bg-color-paper"
-                      onClick={() => setOpen(true)}
-                    >
-                      <span className="m-0 p-0 min-h-fit">{periodScale} </span>
-                      <ChevronDownIcon className="ml-2 h-4 w-4" />
-                    </div>
-                  </div>
-                </PopoverTrigger>
-                <PopoverContent className="flex flex-col w-[100px] bg-color-paper-darker">
-                  {periodScaleOptions.map((periodScale) => {
-                    return (
-                      <button
-                        key={periodScale}
-                        className="text-start p-2 hover:bg-color-paper"
-                        onClick={() => {
-                          setOpen(false);
-                          setPeriodScale(periodScale);
-                        }}
-                      >
-                        {periodScale}{" "}
-                      </button>
-                    );
-                  })}
-                </PopoverContent>
-              </Popover>
-            </div>
-          </ContentWrapper>
-          <ButtonPrimary type="submit" onClick={handleSubmit(onSubmit)}>
-            {/* <span>{context?.hookToEdit ? "Edit Hook" : "Add hook"}</span> */}
-            Add Hook
-          </ButtonPrimary>
-        </Wrapper>
-      </Form>
-      {/* )} */}
+              <br />
+              <PeriodWithScaleInput
+                periodScaleOptions={periodScaleOptions}
+                namePeriodValue="period"
+                namePeriodScale="periodScale"
+                label="Period"
+                validation={{ valueAsNumber: true }}
+              />
+            </ContentWrapper>
+            <ButtonPrimary type="submit" onClick={handleSubmit(onSubmit)}>
+              <span>{context?.hookToEdit ? "Edit Hook" : "Add hook"}</span>
+            </ButtonPrimary>
+          </Wrapper>
+        </Form>
+      )}
     </>
   );
 }
---
+++ apps/create-vesting/src/form-ui/PeriodWithScaleInput.tsx
@@ -0,0 +1,113 @@
+import { InfoTooltip } from "@bleu/cow-hooks-ui";
+import {
+  cn,
+  Input,
+  Label,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@bleu/ui";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { HTMLProps, useState } from "react";
+import {
+  FieldError,
+  RegisterOptions,
+  useFormContext,
+  useFormState,
+  useWatch,
+} from "react-hook-form";
+
+interface IPeriodWithScaleInput
+  extends Omit<HTMLProps<HTMLInputElement>, "name"> {
+  periodScaleOptions: string[];
+  namePeriodValue: string;
+  namePeriodScale: string;
+  label?: string;
+  tooltipText?: string;
+  tooltipLink?: string;
+  extraLabelElement?: React.ReactNode;
+  validation?: RegisterOptions;
+}
+
+export function PeriodWithScaleInput({
+  periodScaleOptions,
+  namePeriodValue,
+  namePeriodScale,
+  label,
+  tooltipText,
+  tooltipLink,
+  extraLabelElement,
+  validation,
+  className,
+  ...props
+}: IPeriodWithScaleInput) {
+  const [open, setOpen] = useState(false);
+
+  const { register, control, setValue } = useFormContext();
+
+  const { errors } = useFormState({ control });
+  const { period, periodScale } = useWatch({ control });
+
+  const error = errors[namePeriodValue] as FieldError | undefined;
+  const errorMessage = error?.message;
+
+  return (
+    <div>
+      {label && (
+        <div className="flex flex-row gap-x-2 items-center mb-2">
+          <Label className="ml-2 block text-sm">{label}</Label>
+          {tooltipText && <InfoTooltip text={tooltipText} link={tooltipLink} />}
+          {extraLabelElement}
+        </div>
+      )}
+      <div className="flex w-48 gap-1">
+        <Input
+          type="number"
+          autoComplete="off"
+          className={cn(
+            "outline-none text-right w-24 h-10 p-2.5 rounded-l-xl rounded-r-none text-base text-color-text-paper border-color-border bg-color-paper-darker [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
+            className
+          )}
+          {...register(namePeriodValue, validation)}
+          {...props}
+        />
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger className="w-full">
+            <div className="flex flex-col">
+              <div
+                className="w-24 h-10 p-2.5 flex items-center justify-between rounded-r-xl text-color-text-paper bg-color-paper-darker"
+                onClick={() => setOpen(true)}
+              >
+                <span className="m-0 p-0 min-h-fit">
+                  {period > 1 ? `${periodScale}s` : periodScale}
+                </span>
+                <ChevronDownIcon className="ml-2 h-4 w-4" />
+              </div>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="flex flex-col w-[100px] bg-color-paper-darker px-0 py-2">
+            {periodScaleOptions.map((periodScale) => {
+              return (
+                <button
+                  key={periodScale}
+                  className="text-start p-2 hover:bg-color-paper"
+                  onClick={() => {
+                    setOpen(false);
+                    setValue(namePeriodScale, periodScale);
+                  }}
+                >
+                  {period > 1 ? `${periodScale}s` : periodScale}
+                </button>
+              );
+            })}
+          </PopoverContent>
+        </Popover>
+      </div>
+      {errorMessage && (
+        <div className="mt-1 ml-2 text-start text-sm text-destructive">
+          {errorMessage}
+        </div>
+      )}
+    </div>
+  );
+}
