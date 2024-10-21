db8c79ae45ecfb7297596a4a737d30211a4551ed
Jean Neiverth <jeanneiverth@gmail.com>
2024-10-21 15:55:51 -0300

    refactor: move scrollbar to cow-hooks-ui and update all scrollbars

--- apps/withdraw-pool/src/app/layout.tsx
+++ apps/withdraw-pool/src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { IFrameContextProvider } from "@bleu/cow-hooks-ui";
+import { IFrameContextProvider, Scrollbar } from "@bleu/cow-hooks-ui";
 import { UserPoolContextProvider } from "#/context/userPools";
 import "@bleu/cow-hooks-ui/global.css";
 import Head from "next/head";
@@ -16,11 +16,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </Head>
       <body>
         <IFrameContextProvider>
-          <div className="font-sans font-normal scrollbar-w-1 scrollbar scrollbar-thumb-color-paper-darkest scrollbar-track-color-paper-darker h-screen overflow-y-scroll important p-[16px] bg-transparent text-color-text">
+          <Scrollbar>
             <UserPoolContextProvider>
               <FormContextProvider>{children}</FormContextProvider>
             </UserPoolContextProvider>
-          </div>
+          </Scrollbar>
         </IFrameContextProvider>
       </body>
     </html>
