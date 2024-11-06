fb028b7e42265203fca87963db10cbd59fd11cf4
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-11-06 11:40:35 -0300

    fix: create vesting vesting amount input mobile view

--- packages/cow-hooks-ui/src/ui/TokenAmountInput.tsx
+++ packages/cow-hooks-ui/src/ui/TokenAmountInput.tsx
@@ -62,22 +62,22 @@ export function TokenAmountInput({
           {extraLabelElement}
         </div>
       )}
-      <div className="flex items-center justify-between w-full min-h-24 py-4 px-6 bg-color-paper-darker rounded-xl">
-        <div className="rounded-xl text-color-text-paper bg-color-paper cursor-default">
-          {token && (
-            <div className="min-w-30 w-fit h-12 px-4 flex items-center justify-center gap-2">
-              <div className="w-6 h-6">
-                <TokenLogo token={token} height={24} width={24} alt="" />
+      <div className="flex flex-col gap-1 w-full min-h-24 pt-4 pb-1 px-6 bg-color-paper-darker rounded-xl items-start">
+        <div className="flex w-full items-center justify-between gap-2 w-full">
+          <div className="rounded-xl text-color-text-paper bg-color-paper cursor-default">
+            {token && (
+              <div className="w-fit py-2 px-4 flex items-center justify-center gap-2">
+                <div className="w-6 h-6">
+                  <TokenLogo token={token} height={24} width={24} alt="" />
+                </div>
+                <span className="m-0 p-0 min-h-fit">{token?.symbol}</span>
               </div>
-              <span className="m-0 p-0 min-h-fit">{token?.symbol}</span>
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col justify-between items-end h-full">
-          {disabled ? (
+            )}
+          </div>
+          {disabled && disabledValue ? (
             <span
               title={disabledValueFullDecimals}
-              className="outline-none text-right p-0 m-0 border-none text-xl text-color-text-paper bg-inherit placeholder:opacity-70 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none cursor-default font-semibold"
+              className="outline-none font-semibold text-xl text-color-text-paper bg-inherit placeholder:opacity-70 text-xl text-color-text-paper bg-inherit placeholder:opacity-70 text-right border-none block overflow-hidden text-ellipsis whitespace-nowrap"
             >
               {shouldDisplayApprox && "≈ "}
               {disabledValue}
@@ -85,39 +85,33 @@ export function TokenAmountInput({
           ) : (
             <Input
               className={cn(
-                "outline-none text-right p-0 m-0 h-min border-none rounded-none text-xl text-color-text-paper bg-inherit placeholder:opacity-70 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none font-semibold",
-                className,
+                "outline-none font-semibold text-xl text-color-text-paper bg-inherit placeholder:opacity-70 text-xl text-color-text-paper bg-inherit placeholder:opacity-70 text-right p-0 m-0 h-min border-none rounded-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none truncate",
+                className
               )}
               {...register(name, validation)}
               {...props}
             />
           )}
-          {userBalance && token?.symbol && shouldEnableMaxSelector && (
-            <span className="font-normal">
+        </div>
+        {userBalance && token?.symbol && (
+          <span className="font-normal pl-1">
+            <span
+              title={userBalanceFullDecimals}
+              className="opacity-40 text-xs"
+            >
+              Balance: {userBalance} {token?.symbol}
+            </span>
+            {shouldEnableMaxSelector && (
               <button
                 type="button"
-                className="inline text-color-text-paper bg-color-paper px-1 mr-1 opacity-100 rounded-md text-xs hover:bg-color-primary hover:text-color-button-text transition-all duration-[200ms] ease-in-out [outline:none]"
+                className="inline text-color-text-paper bg-color-paper px-1 ml-1 opacity-100 rounded-md text-xs hover:bg-color-primary hover:text-color-button-text transition-all duration-[200ms] ease-in-out [outline:none]"
                 onClick={() => setValue(name, Number(userBalanceFullDecimals))}
               >
                 MAX
               </button>
-              <span
-                title={userBalanceFullDecimals}
-                className="opacity-40 text-xs cursor-default text-right"
-              >
-                Balance: {userBalance} {token?.symbol}
-              </span>
-            </span>
-          )}
-          {userBalance && token?.symbol && !shouldEnableMaxSelector && (
-            <span
-              title={userBalanceFullDecimals}
-              className="opacity-40 text-xs cursor-default"
-            >
-              Balance: {userBalance} {token?.symbol}
-            </span>
-          )}
-        </div>
+            )}
+          </span>
+        )}
       </div>
       {errorMessage && (
         <div className="mt-1 ml-2 text-start text-sm text-destructive">
