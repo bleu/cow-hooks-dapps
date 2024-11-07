a2b5b40fc01d10d61d57fffdf24e431595381d43
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-11-07 10:36:41 -0300

    fix: use drpcs instead of public rpcs

--- packages/utils/transactionFactory/rpcs.ts
+++ packages/utils/transactionFactory/rpcs.ts
@@ -1,9 +1,22 @@
 import { SupportedChainId } from "@cowprotocol/cow-sdk";
 import { arbitrum, gnosis, mainnet } from "viem/chains";
 
-export const RPC_URL_MAPPING = {
-  [SupportedChainId.MAINNET]: mainnet.rpcUrls.default.http[0],
-  [SupportedChainId.GNOSIS_CHAIN]: gnosis.rpcUrls.default.http[0],
-  [SupportedChainId.ARBITRUM_ONE]: arbitrum.rpcUrls.default.http[0],
-  [SupportedChainId.SEPOLIA]: "https://ethereum-sepolia-rpc.publicnode.com",
-};
+export const RPC_URL_MAPPING =
+  process.env.NODE_ENV === "development"
+    ? {
+        [SupportedChainId.MAINNET]: mainnet.rpcUrls.default.http[0],
+        [SupportedChainId.GNOSIS_CHAIN]: gnosis.rpcUrls.default.http[0],
+        [SupportedChainId.ARBITRUM_ONE]: arbitrum.rpcUrls.default.http[0],
+        [SupportedChainId.SEPOLIA]:
+          "https://ethereum-sepolia-rpc.publicnode.com",
+      }
+    : {
+        [SupportedChainId.MAINNET]:
+          "https://lb.drpc.org/ogrpc?network=ethereum&dkey=AnOfyGnZ_0nWpS-OOwQzqAnACrGNjLcR77k8TgFkVp5j",
+        [SupportedChainId.GNOSIS_CHAIN]:
+          "https://lb.drpc.org/ogrpc?network=gnosis&dkey=AnOfyGnZ_0nWpS-OOwQzqAnACrGNjLcR77k8TgFkVp5j",
+        [SupportedChainId.ARBITRUM_ONE]:
+          "https://lb.drpc.org/ogrpc?network=arbitrum&dkey=AnOfyGnZ_0nWpS-OOwQzqAnACrGNjLcR77k8TgFkVp5j",
+        [SupportedChainId.SEPOLIA]:
+          "https://lb.drpc.org/ogrpc?network=sepolia&dkey=AnOfyGnZ_0nWpS-OOwQzqAnACrGNjLcR77k8TgFkVp5j",
+      };
