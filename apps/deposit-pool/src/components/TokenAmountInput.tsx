97b10370081a9b75e4b3be503c45d8894671391e
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-11-12 15:15:44 -0300

    remove trailling zeros from user balance

--- apps/deposit-pool/src/components/TokenAmountInput.tsx
+++ apps/deposit-pool/src/components/TokenAmountInput.tsx
@@ -105,7 +105,7 @@ export function TokenAmountInput({
                   "decimal",
                   "standard",
                   0.0001,
-                )}
+                ).replace(/\.?0+$/, "")}
               </span>
               {!buttonDisabled && (
                 <Button
