c077100b0df9a9e7ea8a29ce4373bb1a5aef2a1b
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-12-12 12:12:18 -0300

    fix: re-render of page

--- apps/withdraw-uni-v2/src/app/page.tsx
+++ apps/withdraw-uni-v2/src/app/page.tsx
@@ -12,7 +12,7 @@ import {
   type WithdrawSchemaType,
   decodeExitPoolHookCalldata,
 } from "@bleu/utils";
-import { useCallback, useEffect, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { useFormContext, useWatch } from "react-hook-form";
 import { PoolForm } from "#/components/PoolForm";
 import { PoolItemInfo } from "#/components/PoolItemInfo";
@@ -25,6 +25,7 @@ export default function Page() {
   const [isEditHookLoading, setIsEditHookLoading] = useState(true);
   const { context } = useIFrameContext();
   const { data: pools, isLoading: isLoadingPools, mutate } = useUserPools();
+
   const { setValue, control } = useFormContext<WithdrawSchemaType>();
 
   const poolId = useWatch({ control, name: "poolId" });
@@ -65,36 +66,47 @@ export default function Page() {
     return <span className="mt-10 text-center">Unsupported chain</span>;
   }
 
-  if (isLoadingPools || (isEditHookLoading && context.hookToEdit)) {
-    return (
-      <div className="text-center mt-10 p-2">
-        <Spinner size="xl" />
-      </div>
-    );
+  if (
+    pools === undefined ||
+    isLoadingPools ||
+    (isEditHookLoading && context.hookToEdit)
+  ) {
+    return <LoadingPage />;
   }
 
   return (
-    <div className="w-full flex flex-col gap-1 items-center">
-      <PoolsDropdownMenu
-        onSelect={(pool: IPool) => {
-          setValue("poolId", pool.id);
-        }}
-        onFetchNewPoolSuccess={(pool: IPool | undefined) => {
-          if (!pool) return;
-          const chainId = context.chainId;
-          const poolWithChainId = { ...pool, chainId };
-          const poolsWithChainId = pools?.map((p) => ({ ...p, chainId })) || [];
-          mutate(combineTokenLists([poolWithChainId], poolsWithChainId));
-        }}
-        pools={pools || []}
-        PoolItemInfo={PoolItemInfo}
-        selectedPool={selectedPool}
-        isCheckDetailsCentered
-        tooltipText="Withdraw of staked liquidity or pool with low user balance are not supported"
-        fetchNewPoolCallback={fetchNewPoolCallback}
-        getPoolLink={getUniswapV2PoolLink}
-      />
-      <PoolForm selectedPool={selectedPool} />
+    <Suspense fallback={<LoadingPage />}>
+      <div className="w-full flex flex-col gap-1 items-center">
+        <PoolsDropdownMenu
+          onSelect={(pool: IPool) => {
+            setValue("poolId", pool.id);
+          }}
+          onFetchNewPoolSuccess={(pool: IPool | undefined) => {
+            if (!pool) return;
+            const chainId = context.chainId;
+            const poolWithChainId = { ...pool, chainId };
+            const poolsWithChainId =
+              pools?.map((p) => ({ ...p, chainId })) || [];
+            mutate(combineTokenLists([poolWithChainId], poolsWithChainId));
+          }}
+          pools={pools || []}
+          PoolItemInfo={PoolItemInfo}
+          selectedPool={selectedPool}
+          isCheckDetailsCentered
+          tooltipText="Withdraw of staked liquidity or pool with low user balance are not supported"
+          fetchNewPoolCallback={fetchNewPoolCallback}
+          getPoolLink={getUniswapV2PoolLink}
+        />
+        <PoolForm selectedPool={selectedPool} />
+      </div>
+    </Suspense>
+  );
+}
+
+function LoadingPage() {
+  return (
+    <div className="text-center mt-10 p-2">
+      <Spinner size="xl" />
     </div>
   );
 }
