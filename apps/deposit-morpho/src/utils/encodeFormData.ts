ebe2239a5ca5880b2f9879557b108d1d45cdb1f0
Jean Neiverth <jeanneiverth@gmail.com>
2025-04-02 18:07:57 -0300

    refactor: encoding callData in deposit morpho

--- apps/deposit-morpho/src/utils/encodeFormData.ts
+++ apps/deposit-morpho/src/utils/encodeFormData.ts
@@ -1,21 +1,18 @@
+import { encodeAbiParameters } from "viem";
 import type { DepositMorphoEncodeData } from "./types";
 
-function remove0x(v: string): string {
-  return v.startsWith("0x") ? v.slice(2) : v;
-}
-
-function processAmount(amount: bigint) {
-  return amount.toString(16).padStart(64, "0");
-}
+export const encodingParams = [
+  { name: "vaultId", type: "address" },
+  { name: "amountBigInt", type: "uint256" },
+] as const;
 
 export function encodeFormData(data: DepositMorphoEncodeData): string {
   const { vaultId, amount } = data;
 
-  const encodedVaultId = remove0x(vaultId); // size = 40
-  const encodedAmount = remove0x(processAmount(amount)); // size = 64
+  const encodedData = encodeAbiParameters(encodingParams, [
+    vaultId as `0x${string}`,
+    amount,
+  ]);
 
-  // total size = 104
-  const result = `${encodedVaultId + encodedAmount}`;
-
-  return result;
+  return encodedData.slice(-64 * encodingParams.length);
 }
