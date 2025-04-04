b4ee974269f682751269eca697d8c46afed91895
Jean Neiverth <jeanneiverth@gmail.com>
2025-04-04 12:00:47 -0300

    fix: duplicated balances diffs on uni-v2 and cow-amm deposits

--- packages/cow-hooks-ui/src/hooks/useTokensAfterSwap.ts
+++ packages/cow-hooks-ui/src/hooks/useTokensAfterSwap.ts
@@ -1,47 +1,75 @@
 import { useMemo } from "react";
-import type { Address } from "viem";
-import { useIFrameContext } from "../context";
-import { updateTokenBalanceAfterSwap } from "../utils/math";
-import { useSwapAmount } from "./useSwapAmount";
+import { type Address, formatUnits } from "viem";
+import { useReadTokenContract } from "./useReadTokenContract";
 import { useTokens } from "./useTokens";
 
 export function useTokensAfterSwap(
   tokens: Address[],
 ): Record<string, { balance: `${number}`; decimals: number; symbol: string }> {
-  const { context } = useIFrameContext();
   const balancesBeforeSwap = useTokens(tokens);
-  const { buyAmount, sellAmount } = useSwapAmount();
+
+  const token0 = tokens.length > 0 ? tokens[0] : undefined;
+  const token1 = tokens.length > 1 ? tokens[1] : undefined;
+
+  const {
+    userBalance: balance0,
+    tokenSymbol: symbol0,
+    tokenDecimals: decimals0,
+  } = useReadTokenContract({ tokenAddress: token0 });
+  const {
+    userBalance: balance1,
+    tokenSymbol: symbol1,
+    tokenDecimals: decimals1,
+  } = useReadTokenContract({ tokenAddress: token1 });
+
+  const floatBalance0 = useMemo(
+    () =>
+      balance0 && decimals0 ? formatUnits(balance0, decimals0) : undefined,
+    [balance0, decimals0],
+  );
+  const floatBalance1 = useMemo(
+    () =>
+      balance1 && decimals1 ? formatUnits(balance1, decimals1) : undefined,
+    [balance1, decimals1],
+  );
+
   return useMemo(() => {
-    if (!buyAmount || !sellAmount || !context?.orderParams)
+    if (
+      !token0 ||
+      !token1 ||
+      !floatBalance0 ||
+      !floatBalance1 ||
+      !symbol0 ||
+      !symbol1 ||
+      !decimals0 ||
+      !decimals1
+    )
       return balancesBeforeSwap;
 
-    return Object.keys(balancesBeforeSwap || {}).reduce(
-      (acc, token, _index) => {
-        const tokenBalance = balancesBeforeSwap[token];
-        if (!tokenBalance) return acc;
-        return {
-          // biome-ignore lint:
-          ...acc,
-          [token]: {
-            balance: updateTokenBalanceAfterSwap({
-              userBalance: balancesBeforeSwap[token].balance,
-              tokenAddress: token as Address,
-              tokenDecimals: balancesBeforeSwap[token].decimals,
-              tokenBuyAddress: context.orderParams?.buyTokenAddress as Address,
-              tokenSellAddress: context.orderParams
-                ?.sellTokenAddress as Address,
-              sellAmount,
-              buyAmount,
-            }),
-            decimals: balancesBeforeSwap[token].decimals,
-            symbol: balancesBeforeSwap[token].symbol,
-          },
-        };
+    return {
+      [token0]: {
+        balance: floatBalance0,
+        decimals: decimals0,
+        symbol: symbol0,
       },
-      {} as Record<
-        string,
-        { balance: `${number}`; decimals: number; symbol: string }
-      >,
-    );
-  }, [balancesBeforeSwap, buyAmount, sellAmount, context?.orderParams]);
+      [token1]: {
+        balance: floatBalance1,
+        decimals: decimals1,
+        symbol: symbol1,
+      },
+    } as Record<
+      string,
+      { balance: `${number}`; decimals: number; symbol: string }
+    >;
+  }, [
+    token0,
+    token1,
+    floatBalance0,
+    floatBalance1,
+    symbol0,
+    symbol1,
+    decimals0,
+    decimals1,
+    balancesBeforeSwap,
+  ]);
 }
